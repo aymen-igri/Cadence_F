f1acf23bf48aaf4be714891cebc40bb33cc6e1a1
0XAymax <aymanderrouich3@gmail.com>
2026-03-07 16:44:00 +0000

    changing some shit

--- src/app/pages/sign-in/sign-in.ts
+++ src/app/pages/sign-in/sign-in.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { LucideAngularModule, Eye, EyeOff, BookOpen } from 'lucide-angular';
 import { HlmButtonImports } from '../../../components/ui/button/src';
 import { HlmInputImports } from '../../../components/ui/input/src';
 import { HlmLabelImports } from '../../../components/ui/label/src';
 import { HlmCardImports } from '../../../components/ui/card/src';
 import { HlmSeparatorImports } from '../../../components/ui/separator/src';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-sign-in',
   imports: [
+    NgOptimizedImage,
     LucideAngularModule,
     ...HlmButtonImports,
     ...HlmInputImports,
     ...HlmLabelImports,
     ...HlmCardImports,
     ...HlmSeparatorImports,
-  ],
+    RouterLink
+],
   templateUrl: './sign-in.html',
   styleUrl: './sign-in.css',
 })
